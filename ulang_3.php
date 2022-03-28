205b100fb2f7c1c3c595e59f3f0f51438e7be204
nikenmn <nikenmaharani24@gmail.com>
2022-03-28 16:40:18 +0700

    commit

---
+++ ulang_3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <?php
+            $i=1;   // inisialisasi
+            while($i<10){ //kondisi true
+                echo "Anda urutan ke-$i <br>";
+                $i++; //perubahan nilai
+            }
+        ?>
+    </body>
+</html>
