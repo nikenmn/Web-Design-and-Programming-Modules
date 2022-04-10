b8b3c008680decde8ea7c9a5f3524e9baa82a9aa
Niken Maharani Permata <90488986+nikenmn@users.noreply.github.com>
2022-04-10 23:24:40 +0700

    Delete postForm.php

--- postForm.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head></head>
-    <body>
-        Selamat Datang <?php echo $_POST["myname"]; ?> !!<br>
-        Dari <?php echo $_POST["myaddress"]; ?>
-    </body>
-</html>
